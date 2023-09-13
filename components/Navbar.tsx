ffb19c51e7a77a3c85d6cc9e9107a20ebdd333b8
Lex Van-Nugent <lexvn.wd@gmail.com>
2023-09-13 13:51:08 -0600

    Create initial components and folder

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          Home
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ components/index.ts
@@ -0,0 +1,11 @@
+// exported components
+
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+};
